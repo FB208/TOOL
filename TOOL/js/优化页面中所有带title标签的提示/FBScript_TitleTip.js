9891556d257f146185d0701eb828ed30396a73e6
杨惠超 <472189065@qq.com>
2015-01-12 11:30:57 +0800

    md5小工具、获取浏览器内核等

---
+++ TOOL/js/优化页面中所有带title标签的提示/FBScript_TitleTip.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+    $hasTitle = $("[title]");
+    var offsetX = 10;
+    var offsetY = 20;
+    $hasTitle.mouseover(function (e) {
+        this.myTitle = this.title;
+        this.title = "";
+        var tip = "<div id='tooltip'>" + this.myTitle + "</div>";
+        $("body").append(tip);
+        $("#tooltip").css(
+            {
+                "position": "absolute",
+                "font-size": "10px",
+                "background-color": "#e9e8a7",
+                "border": "solid",
+                "border-width": "1px",
+                "top": (e.pageY + offsetY) + "px",
+                "left": (e.pageX + offsetX) + "px"
+            });
+
+    }).mouseout(function () {
+        this.title = this.myTitle;
+        $("#tooltip").remove();
+    }).mousemove(function (e) {
+        //alert(e.pageX);
+        $("#tooltip").css({
+            "top": (e.pageY + offsetY) + "px",
+            "left": (e.pageX + offsetX) + "px"
+        });
+    });
+});
